8fa4111b6f09131e4cb9473ed2030d682f9b2a83
totsamiydrug <123543836+totsamiydrug@users.noreply.github.com>
2023-11-13 02:02:24 +0300

    Add files via upload

---
+++ LR1.cpp
@@ -0,0 +1,102 @@
+﻿#include <iostream>
+#include <vector>
+#include <iomanip>
+#include <numeric>
+#include <algorithm>
+
+void disp(const std::vector<double>& arr) {
+
+  for (double x : arr) {
+    std::cout << std::fixed << std::setprecision(3) << x << " ";
+  }
+  std::cout << std::endl;
+
+}
+
+void add_element(std::vector<double>& arr, double &sum, double &avg) {
+
+  sum = accumulate(arr.begin(), arr.end(), 0.0);
+  avg = sum / arr.size();
+  arr.push_back(sum);
+  arr.push_back(avg);
+
+}
+
+void rabotaSVektorom(std::vector<double>& arr, int n, double& avg) {
+
+  int flag = 0;
+  for (int i = 0; i < arr.size(); i++) {
+    if (arr[i] > 0) {
+      flag++;
+      if (flag == 3) {
+        arr[i] = avg;
+        flag = 0;
+      }
+    }
+  }
+
+}
+
+int menu(std::vector<double>& arr, int n, double &sum, double &avg) {
+
+  int choice;
+  while (true) {
+      std::cin >> choice;
+    switch (choice) {
+    case 0:
+      return 0;
+    case 1:
+      double elem;
+      std::cin >> elem;
+      arr.push_back(elem);
+      std::cout << "+: " << arr.size() << std::endl;
+      disp(arr);
+      add_element(arr, sum, avg);
+      disp(arr);
+      rabotaSVektorom(arr, n, avg);
+      disp(arr);
+      break;
+    case 2:
+      if (!arr.empty()) {
+        arr.pop_back();
+        std::cout << "-: " << arr.size() << std::endl;
+        disp(arr);
+        add_element(arr, sum, avg);
+        disp(arr);
+        rabotaSVektorom(arr, n, avg);
+        disp(arr);  
+      }
+      else {
+          std::cout << "Массив пуст, удаление невозможно." << std::endl;
+      }
+      break;
+    default:
+        std::cout << "Неверный выбор действия. Попробуйте еще раз." << std::endl;
+    }
+  }
+
+}
+
+int main() {
+
+  setlocale(LC_ALL, "Russian");
+  double  sum = 0;
+  double avg = 0;
+  int n;
+  std::cin >> n;
+  std::vector<double> arr(n);
+  for (int i = 0; i < n; i++) {
+      std::cin >> arr[i];
+  }
+
+  std::cout << std::endl << n << std::endl;
+
+  disp(arr);
+  add_element(arr, sum, avg);
+  disp(arr);
+  rabotaSVektorom(arr, n, avg);
+  disp(arr);
+  menu(arr, n, sum, avg);
+  return 0;
+
+}
