5567ce259fb76a89f777b984d57dee87d90c9a8b
totsamiydrug <123543836+totsamiydrug@users.noreply.github.com>
2024-04-11 12:08:32 +0300

    Создание администратора

---
+++ admin/includes/sidebar.php
@@ -0,0 +1,75 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Администратор</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    <li class="no-padding"><a class="waves-effect waves-grey" href="dashboard.php"><i class="material-icons">settings_input_svideo</i>Информационная панель</a></li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Отделы<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Добавить отдел</a></li>
+                                <li><a href="managedepartments.php">Управление отделами</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">code</i>Типы отпусков<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addleavetype.php">Добавить тип отпуска</a></li>
+                                <li><a href="manageleavetype.php">Управление</a></li>
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Сотрудники<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Добавить сотрудника</a></li>
+                                <li><a href="manageemployee.php">Управление сотрудниками</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+   <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">desktop_windows</i>Управление отпусками<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="leaves.php">Все отпуска </a></li>
+                                <li><a href="pending-leavehistory.php">На рассмотрении</a></li>
+                                <li><a href="approvedleave-history.php">Утвержденые отпуска</a></li>
+                                  <li><a href="notapproved-leaves.php">Не утвержденые отпуска</a></li>
+       
+                            </ul>
+                        </div>
+                    </li>
+
+
+
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Выйти</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                    <p class="copyright"><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">FARID AZIZOV </a>©</p>
+                
+                </div>
+                </div>
+            </aside>
