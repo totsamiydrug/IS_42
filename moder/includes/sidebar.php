06b499365d5ee9590ec7a8ca9237d94bf923e11c
totsamiydrug <123543836+totsamiydrug@users.noreply.github.com>
2024-04-11 12:15:44 +0300

    Модератор
    Создана база модератора

---
+++ moder/includes/sidebar.php
@@ -0,0 +1,64 @@
+     <aside id="slide-out" class="side-nav white fixed">
+                <div class="side-nav-wrapper">
+                    <div class="sidebar-profile">
+                        <div class="sidebar-profile-image">
+                            <img src="../assets/images/profile-image.png" class="circle" alt="">
+                        </div>
+                        <div class="sidebar-profile-info">
+                       
+                                <p>Модератор</p>
+
+                         
+                        </div>
+                    </div>
+            
+                <ul class="sidebar-menu collapsible collapsible-accordion" data-collapsible="accordion">
+                    
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">apps</i>Отделы<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="adddepartment.php">Добавить отдел</a></li>
+                               
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">code</i>Типы отпусков<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addleavetype.php">Добавить тип отпуска</a></li>
+                               
+                            </ul>
+                        </div>
+                    </li>
+                    <li class="no-padding">
+                        <a class="collapsible-header waves-effect waves-grey"><i class="material-icons">account_box</i>Сотрудники<i class="nav-drop-icon material-icons">keyboard_arrow_right</i></a>
+                        <div class="collapsible-body">
+                            <ul>
+                                <li><a href="addemployee.php">Добавить сотрудника</a></li>
+                                
+       
+                            </ul>
+                        </div>
+                    </li>
+
+  
+
+
+
+
+                        <li class="no-padding">
+                                <a class="waves-effect waves-grey" href="logout.php"><i class="material-icons">exit_to_app</i>Выйти</a>
+                            </li>  
+                 
+
+                 
+              
+                </ul>
+                   <div class="footer">
+                    <p class="copyright"><a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">FARID AZIZOV </a>©</p>
+                
+                </div>
+                </div>
+            </aside>
